0068bb0eea286479ab238332d0690bd09dc7a5fb
bhargavi <122342419+bhxrg@users.noreply.github.com>
2023-06-20 19:20:03 +0530

    Add files via upload

---
+++ boxplot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+
+# Sample data
+data = [10, 12, 15, 17, 18, 20, 21, 22, 25, 28, 30]
+
+# Create box plot
+plt.boxplot(data)
+
+# Set title and labels
+plt.title("Box Plot")
+plt.xlabel("Data")
+plt.ylabel("Values")
+
+# Display the plot
+plt.show()
+
+
+# In[ ]:
+
+
+
+
